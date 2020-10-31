0c8723dda20ac010d15fafd15e057db63cc0a4ee
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2020-11-01 01:03:49 +0700

    Scoring

---
+++ Assets/Scripts/CoinScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoinScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D coinobj)
+    {
+        if (coinobj.gameObject.CompareTag("Coin"))
+        {
+            Destroy(coinobj.gameObject);
+            uiCoin.scoremanager.UpdateSCore(1);
+        }
+
+        if (coinobj.gameObject.CompareTag("Gem"))
+        {
+            Destroy(coinobj.gameObject);
+            uiCoin.scoremanager.UpdateSCore(10);
+        }
+    }
+}
---
+++ Assets/Scripts/uiCoin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class uiCoin : MonoBehaviour
+{
+    public static uiCoin scoremanager;
+    int score;
+    public TextMeshProUGUI scr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scr.text = "x0";
+        if (scoremanager == null)
+        {
+            scoremanager = this;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateSCore(int s)
+    {
+        score += s;
+        scr.text = "x" + score.ToString();
+    }
+}
