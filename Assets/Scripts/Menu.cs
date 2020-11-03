536e08234e26a074d5abb9d79c357de50624d170
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2020-11-03 15:43:31 +0700

    Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,5 +9,10 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGmae()
+    {
+        Application.Quit();
+    }
 }
  
